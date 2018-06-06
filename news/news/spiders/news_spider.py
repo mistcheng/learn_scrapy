4a4c29d4fb13426227deefce2f868c5e7b8df0f8
wind <iyuanfang0105@gmail.com>
2018-06-06 19:22:44 +0800

    news update

--- news/news/spiders/news_spider.py
+++ news/news/spiders/news_spider.py
@@ -1,8 +1,11 @@
+import os
 import scrapy
-
+from io import open
 
 class NewsSpider(scrapy.Spider):
     name = "news"
+
+    write_path = '/Users/wind/WORK/code/learn_scrapy/data'
 
     def start_requests(self):
         urls = [
@@ -32,7 +35,46 @@ class NewsSpider(scrapy.Spider):
 
     def parse_1(self, response):
         self.log('++++====>>>> get information from page: %s' % response.url)
-        title = response.css('div.yc_tit > h1::text').extract_first()
-        # content = response.css('div.yc_con')
 
-        self.log('++++====>>>> %s' % title)
+        title = response.css('div.yc_tit > h1::text').extract_first()
+        contents = response.css('div.yc_con_txt > p')
+        time = response.css('div.yc_tit > p > span::text').extract_first()
+        source = response.css('div.yc_tit > p > a::text').extract_first()
+        # pic = response.css('div.yc_con_txt > p.detalPic > img::attr(src)').extract_first()
+
+        if title is None:
+            title = response.css('div#artical > h1::text').extract_first()
+            contents = response.css('div#main_content.js_selection_area > p')
+            time = response.css('div#artical_sth > p.p_time > span[itemprop="datePublished"]::text').extract_first()
+            source = response.css('div#artical_sth > p.p_time > span[itemprop="publisher"] > span[itemprop="name"]> a::text').extract_first()
+            # pic = response.css('div#main_content.js_selection_area > p.detalPic > img::attr(src)').extract_first()
+
+        pic = None
+        for p in contents:
+            temp = p.css('p.detailPic > img::attr(src)').extract_first()
+            if temp is not None:
+                pic = temp
+
+        contents_txt = ''
+        temp = []
+        for cont in contents:
+            paragraph = cont.css('p::text').extract_first()
+            if paragraph is not None:
+                temp.append(paragraph)
+        if len(temp) > 0:
+            contents_txt = '\n'.join(c for c in temp)
+
+        if title is not None:
+            # self.log('++++====>>>> %s' % title)
+            # self.log('++++====>>>> %s' % contents_txt)
+            self.log('++++====>>>> %s' % time.strip())
+            self.log('++++====>>>> %s' % source.strip())
+            self.log('++++====>>>> %s' % pic)
+
+            # self.write_file(self.write_path, 'ifeng_top_news', contents_txt)
+
+    def write_file(self, write_path, file_name, content):
+        file_path = os.path.join(write_path, file_name)
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(content)
+            self.log('Saved file %s' % file_path)
